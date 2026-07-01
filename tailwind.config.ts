bd4f3f477f26b743ea89733d6596a2dd8b641f41
BillReader <wreader@ford.com>
2026-07-01 16:58:46 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,4 +10,14 @@ const config: Config = {
     extend: {
       colors: {
         'status-green': '#22c55e',
-        '
+        'status-yellow': '#eab308',
+        'status-red': '#ef4444',
+        'status-gray': '#9ca3af',
+        'ford-blue': '#003478',
+        'ford-blue-light': '#1a5dab',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
